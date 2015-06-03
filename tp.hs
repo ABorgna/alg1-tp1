08ff5001e5be60db8e108c60e83c1292604bed1d
Agustin Borgna <hola@aborgna.com.ar>
2015-06-03 16:19:27 -0300

    Esqueleto de las funciones

---
+++ tp.hs
@@ -0,0 +1,47 @@
+
+-- | 1
+-- Funciones auxiliares
+esMin :: Char -> Bool
+esMin = undefined
+
+letANat :: Char -> Integer
+letANat = undefined
+
+natALet :: Integer -> Char
+natALet = undefined
+
+desplazar :: Integer -> Char -> Char
+desplazar = undefined
+
+cantMinusc :: String -> Integer
+cantMinusc = undefined
+
+contar :: Char -> String -> Integer
+contar = undefined
+
+-- | 2
+-- Encriptar con cifrado Cesar
+codificar :: Integer -> String -> String
+codificar = undefined
+
+-- | 3
+decodificar :: Integer -> String -> String
+decodificar = undefined
+
+-- | 4
+frec :: String -> [Float]
+frec = undefined
+
+-- | 5
+rotar :: Integer -> [a] -> [a]
+rotar = undefined
+
+-- | 6
+-- Distancia
+chi2 :: [Float] -> [Float] -> Float
+chi2 = undefined
+
+-- | 7
+descifrar :: String -> String
+descifrar = undefined
+
