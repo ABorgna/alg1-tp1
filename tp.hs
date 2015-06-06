02c425f9e7694293c6b3f7bceb3f578e4df39783
Agustin Borgna <hola@aborgna.com.ar>
2015-06-06 18:53:30 -0300

    Dejar letANat indefinida para letras no minusculas

--- tp.hs
+++ tp.hs
@@ -8,13 +8,12 @@ esMin c = isAsciiLower c
 
 letANat :: Char -> Integer
 letANat c | c `elem` ['a'..'z'] = fromIntegral $ ord c - ord 'a'
-          | otherwise = 0
 
 natALet :: Integer -> Char
 natALet n | 0 <= n && n <= 26 = chr $ fromIntegral n + ord 'a'
 
 desplazar :: Integer -> Char -> Char
-desplazar n c = natALet $ (n + letANat c) `mod` 27
+desplazar n c = natALet $ (n + letANat c) `mod` 26
 
 cantMinusc :: String -> Integer
 cantMinusc s = fromIntegral $ length $ filter esMin s
